a6c4cffc2f132ae8bf684f3e013fb1202f0fdf24
clock <zhong.clock@gmail.com>
2022-08-01 15:04:55 +0800

    some changes

--- clean.sh
+++ clean.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
 rm -fr Makefile.in aclocal.m4 autom4te.cache build compile config.guess config.h.in config.sub configure depcomp install-sh man/Makefile.in missing
+rm -fr .vscode/dryrun.log .vscode/targets.log
